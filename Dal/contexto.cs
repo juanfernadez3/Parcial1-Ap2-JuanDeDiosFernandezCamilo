f4be19aa405b7a83467d690692289ef1d89dd188
Juan De Dios Fernandez Camilo <juan_fernandez3@ucne.edu.do>
2020-06-10 20:42:27 -0400

    se añadio el contexto

---
+++ Dal/contexto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Parcial1_Ap2_JuanDeDiosFernandezCamilo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Parcial1_Ap2_JuanDeDiosFernandezCamilo.Dal
+{
+    public class contexto : DbContext
+    {
+        public DbSet<Articulos> Articulos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data source = DB/Articulos.db");
+        }
+    }
+}
